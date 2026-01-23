7ec66b413700232129bfc3b90908fa5b8c1b92ed
Rishab Raj <rr0948555@gmail.com>
2026-01-23 21:54:45 +0530

    Store

--- src/Redux/Reducer/reducer.js
+++ src/Redux/Reducer/reducer.js
@@ -1,4 +1,4 @@
-import { ADD_TODO, TOGGLE_TODO } from "../Action/actionTypes"
+import { ADD_TODO, FILTER_TODO, MARK_ALL_COMPLETED_TODO, MARK_COMPLETED_TODO, MARK_INCOMPLETE_TODO, REMOVE_TODO, SEARCH_TODO, TOGGLE_TODO } from "../Action/actionTypes"
 
 const initialState = {
     todos: [],
@@ -8,7 +8,7 @@ const initialState = {
 
 const todoReducer = (state = initialState, action) => {
      switch(action.type){
-        case ADD_TODO
+        case ADD_TODO:
           return {
              todos: [...state.todos, {
                 text: action.payLoad.text, completed: false
@@ -16,7 +16,7 @@ const todoReducer = (state = initialState, action) => {
              filter: state.filter,
              search: state.search
           }
-          case TOGGLE_TODO
+          case TOGGLE_TODO:
            return {
                 todos: state.todos.map((todo, index)=> index === action.payLoad.id ? 
                  {...todo, completed: !todo.completed} : todo
@@ -24,5 +24,51 @@ const todoReducer = (state = initialState, action) => {
                filter: state.filter,
                search: state.search
           }
+          case REMOVE_TODO:
+           return {
+                todos: state.todos.filter((todo, index)=> index !== action.payLoad.id
+               ),
+               filter: state.filter,
+               search: state.search
+          }
+          case MARK_COMPLETED_TODO:
+            return {
+               todos: state.todos.map((todo, index) => index === action.payLoad.id ? {...todo, completed: true} : todo
+            ),
+            filter: state.filter,
+            search: state.search
+         }
+          case MARK_INCOMPLETE_TODO:
+            return {
+               todos: state.todos.map((todo, index) => index === action.payLoad.id ? {...todo, completed: false} : todo
+            ),
+            filter: state.filter,
+            search: state.search
+         }
+         case FILTER_TODO:
+            return {
+                todos: state.todos,
+                filter: action.payLoad.filter,
+                search: state.search
+         }
+         case SEARCH_TODO:
+           return {
+              todos: state.todos,
+              filter: state.filter,
+              search: action.payLoad.search
+         }
+         case MARK_ALL_COMPLETED_TODO:
+            return {
+               todos: state.todos.map((todo) => 
+                  ({
+                     ...todo, completed: true
+                  })
+               ),
+               filter: state.filter,
+               search: state.search
+            }
+            default: 
+             return state;
      }
 }
+export default todoReducer;
