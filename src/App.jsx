5c38a221d6f828b658a85303389be0fe8158cae8
Rishab Raj <rr0948555@gmail.com>
2026-01-24 14:08:02 +0530

    Todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Todo from './components/Todo'
+import { Provider } from 'react-redux'
+import store from './Redux/Store/store'
 
 const App = () => {
   return (
-   <Provider>
+   <Provider store={store}>
       <Todo />
    </Provider>
   )
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux"
 
 const Todo = () => {
+  const dispatch = useDispatch();
+  const [todoText, setTodoText] = useState();
   return (
-    <div>
-       <h1>Todo App</h1>
+    <div className="max-w-4xl mx-auto sm:mt-8 p-4 bg-blue-200 rounded">
+       <h2 className="mt-3 mb-6 text-6xl font-bold text-center text-blue-700 uppercase">Todo List App</h2>
+       {/* Input Button */}
+       <div className="Enter Your Task">
+         <input
+           type="text"
+           placeholder="Enter Your Task"
+           className="flex-grow p-2 border-b-2 border-gray-300 rounded focus:outline none focus:border-blue-500"
+           name="text"
+           id="addTodo"
+         />
+       </div>
     </div>
   )
 }
